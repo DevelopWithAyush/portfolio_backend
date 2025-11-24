50372923d466bc03a7b1c2960b05cf75d4d6c640
DevelopWithAyush <developwithdubey@gmail.com>
2025-11-24 08:14:18 +0530

    fixing things

--- src/index.js
+++ src/index.js
@@ -14,8 +14,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    maxAge: 3600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    exposedHeaders: ['Content-Range', 'X-Total-Count'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
